89e970027f5258fbbadd11a55898af7e7c1e981c
wang2478327261 <2478327261@qq.com>
2015-11-04 18:46:53 +0800

    ERRON 20151104

---
+++ src/smartcollision/SmartCollision.java
@@ -0,0 +1,54 @@
+
+package smartcollision;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+public class SmartCollision extends JFrame{
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Ship ship =new Ship(DataBase.defaultx, DataBase.defaulty, 
+                DataBase.defaults, DataBase.defaultc);
+        DyObstacle obstacle = new DyObstacle(DataBase.defaultx, DataBase.defaulty,
+                DataBase.defaults, DataBase.defaultc);
+        
+        Show show = new Show(ship, obstacle);
+        show.repaint();
+        
+        frame.setSize(1024, 730);
+        frame.add(show);
+        frame.setTitle("ONE STEP V2");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+        while (true) { //temp for debug and has a direct view          
+            show.repaint();
+            ship.goAhead();
+            for(DyObstacle o : DataBase.obstacle){
+                o.goAhead();
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(SmartCollision.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+    }
+    
+    
+    public class freshshow implements Runnable{
+
+        Thread freshThread;
+        public freshshow() {
+            
+        }
+        
+        @Override
+        public void run(){
+            
+        }
+    }
+}
