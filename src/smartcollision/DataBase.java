c5112d9ded46eab401184daaf24321632412e1fb
wang2478327261 <2478327261@qq.com>
2015-11-04 18:47:20 +0800

    ERRON 20151104

---
+++ src/smartcollision/DataBase.java
@@ -0,0 +1,22 @@
+
+package smartcollision;
+
+import java.util.ArrayList;
+
+public class DataBase {
+    
+    //default x, y, s, c    store objects
+    public static double defaultx = 300, defaulty = 400, defaults = 15, defaultc = 80;
+    
+    public static ArrayList<DyObstacle> obstacle = new ArrayList<>();//should clear count if count is too large
+    
+    public static int dirpointradius = 10;
+    public static int obstacleradius = 10;
+    //flags
+    public static int danger;//flag of ship is danger
+    
+    
+    /***************************************************************************/
+    //caculate area
+    
+}
